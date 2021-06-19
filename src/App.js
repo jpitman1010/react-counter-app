29f5c5a6d41c8ae2bcc69c5f54fa4f8d6e1d41e8
jpitman1010 <jpitman1010@gmail.com>
2021-06-19 09:01:06 -0700

    componentDidMount and componentDidUpdate - example to show difference for next lesson: useEffects

--- src/App.js
+++ src/App.js
@@ -1,35 +1,91 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "tailwindcss/tailwind.css";
 
+//using componentDidMount and componentDidUpdate to show differences
+//between using this and next lesson: useEffect.  useEffect is cleaner, 
+//better code to use.
+class App extends Component {
 
-const App = () => {
-
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1)
+  state = {
+    count: 0
+  };
+ increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+ decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
   };
 
-  const decrement = () => {
-    setCount(count - 1)
+  componentDidMount() {
+    document.title = `Clicked ${this.state.count} times.`
+    //this only runs the very first time, but when state is updated,
+    //you would have to use componentDidUpdate
   };
+  componentDidUpdate() {
+    document.title = `Clicked ${this.state.count} times.`
+  };
+  //these changes can be seen in the title(in header) of the document.
 
-  return (
+render()  {
+    return (
+      <div >
+        <div><h2>Counter App</h2></div>
+        <div style={{paddingLeft:10, paddingRight:10, paddingTop:4, paddingBottom:4, margin:20}}><button onClick={this.increment}>Increase Counter</button></div>
+        <div style={{paddingLeft:10, paddingRight:10, paddingTop:4, paddingBottom:4, margin:20}}><button onClick={this.decrement}>Decrease Counter</button></div>
+        <div>Clicked {this.state.count} times.</div>
+      </div>
 
-    <div >
-      <div><h2>Counter App</h2></div>
-      <div style={{paddingLeft:10, paddingRight:10, paddingTop:4, paddingBottom:4, margin:20}}>
-        <button onClick={increment}>
-          Increase Counter
-        </button></div>
-      <div style={{paddingLeft:10, paddingRight:10, paddingTop:4, paddingBottom:4, margin:20}}>
-        <button onClick={decrement}>
-          Decrease Counter
-        </button></div>
-      <div>Clicked {count} times.</div>
-    </div>
-
-  );
-};
+    )};
+}
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const App = () => {
+
+//   const [count, setCount] = useState(0);
+
+//   const increment = () => {
+//     setCount(count + 1)
+//   };
+
+//   const decrement = () => {
+//     setCount(count - 1)
+//   };
+
+//   return (
+
+//     <div >
+//       <div><h2>Counter App</h2></div>
+//       <div style={{paddingLeft:10, paddingRight:10, paddingTop:4, paddingBottom:4, margin:20}}>
+//         <button onClick={increment}>
+//           Increase Counter
+//         </button></div>
+//       <div style={{paddingLeft:10, paddingRight:10, paddingTop:4, paddingBottom:4, margin:20}}>
+//         <button onClick={decrement}>
+//           Decrease Counter
+//         </button></div>
+//       <div>Clicked {count} times.</div>
+//     </div>
+
+//   );
+// };
+
+// export default App;
