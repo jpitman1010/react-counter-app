52ccb912b5c3a7e64a6c8d10e6836e5cb705e965
jpitman1010 <jpitman1010@gmail.com>
2021-06-18 18:26:15 -0700

    implimented same with useState, non class based

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React, { Component } from "react";
-import "tailwindcss/tailwind.css"
+import React, { useState } from "react";
+import "tailwindcss/tailwind.css";
 
 
+const App = () => {
 
-class App extends Component {
-  
-  state = {
-    count: 0
-  };
- increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  };
- decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1)
   };
 
-render()  {
-    return (
-      <div >
-        <div><h2>Counter App</h2></div>
-        <div style={{paddingLeft:10, paddingRight:10, paddingTop:4, paddingBottom:4, margin:20}}><button onClick={this.increment}>Increase Counter</button></div>
-        <div style={{paddingLeft:10, paddingRight:10, paddingTop:4, paddingBottom:4, margin:20}}><button onClick={this.decrement}>Decrease Counter</button></div>
-        <div>Clicked {this.state.count} times.</div>
-      </div>
+  const decrement = () => {
+    setCount(count - 1)
+  };
 
-    )};
-}
+  return (
+
+    <div >
+      <div><h2>Counter App</h2></div>
+      <div style={{paddingLeft:10, paddingRight:10, paddingTop:4, paddingBottom:4, margin:20}}>
+        <button onClick={increment}>
+          Increase Counter
+        </button></div>
+      <div style={{paddingLeft:10, paddingRight:10, paddingTop:4, paddingBottom:4, margin:20}}>
+        <button onClick={decrement}>
+          Decrease Counter
+        </button></div>
+      <div>Clicked {count} times.</div>
+    </div>
+
+  );
+};
 
 export default App;
